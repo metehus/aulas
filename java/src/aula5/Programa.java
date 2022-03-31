fcf4ac63ece683b6a8fda82c693eede35864fda1
Matheus <methidezeeni@gmail.com>
2022-03-31 11:24:37 -0300

    Exercicio

---
+++ java/src/aula5/Programa.java
@@ -0,0 +1,13 @@
+package aula5;
+
+public class Programa {
+	public static void main(String[] args) {
+		System.out.println("Aula 5");
+		
+		String frase = "Só sei que nada sei (Sócrates).";
+		
+		for (int i = 0; i < frase.length(); i++) {
+			System.out.println(frase.charAt(i));
+		}
+	}
+}
